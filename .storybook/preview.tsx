fea5ac69528abbd4245f84e58b20ec0a33a158c6
y-natani <yn1323@gmail.com>
2025-08-24 01:23:34 +0900

    fix: 雑にCI移行

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import type { Preview } from "@storybook/nextjs";
+import type { Preview } from "@storybook/nextjs-vite";
 // biome-ignore lint/correctness/noUnusedImports: temp
 import React from "react";
 import { z } from "zod";
