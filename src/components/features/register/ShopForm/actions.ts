f4c1197c651c26da4b98c0fc81074755f37091de
y-natani <yn1323@gmail.com>
2025-08-25 23:10:18 +0900

    fix: lint

--- src/components/features/register/ShopForm/actions.ts
+++ src/components/features/register/ShopForm/actions.ts
@@ -2,6 +2,7 @@
 
 import type { SchemaType } from "./schema";
 
+// biome-ignore lint/correctness/noUnusedFunctionParameters: tmp
 export const registerShop = async (data: SchemaType) => {
   const success = true;
 
--- src/components/features/register/UserForm/actions.ts
+++ src/components/features/register/UserForm/actions.ts
@@ -2,6 +2,7 @@
 
 import type { SchemaType } from "./schema";
 
+// biome-ignore lint/correctness/noUnusedFunctionParameters: temp
 export const registerUser = async (userId: string, { userName }: SchemaType) => {
   const success = true;
 
