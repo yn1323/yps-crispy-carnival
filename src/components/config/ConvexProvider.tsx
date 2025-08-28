a6162e1adc3ef47d6e3894d1027a08b959e72d3b
y-natani <yn1323@gmail.com>
2025-08-28 23:51:38 +0900

    fix: ci

--- src/components/config/ConvexProvider.tsx
+++ src/components/config/ConvexProvider.tsx
@@ -3,7 +3,7 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import type { ReactNode } from "react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL ?? "");
+const convex = new ConvexReactClient(process.env.STORYBOOK_CONVEX_URL ?? process.env.NEXT_PUBLIC_CONVEX_URL ?? "");
 
 export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
   return <ConvexProvider client={convex}>{children}</ConvexProvider>;
