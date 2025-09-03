ff5ad5c307a909705d1ceb649335c48b74d0ea64
y-natani <yn1323@gmail.com>
2025-09-04 01:48:55 +0900

    fix; edge

--- app/(auth)/attendance/page.tsx
+++ app/(auth)/attendance/page.tsx
@@ -2,6 +2,8 @@ import { Center, Heading } from "@chakra-ui/react";
 import { Animation } from "@/src/components/templates/Animation";
 import { verifySession } from "@/src/helpers/utils/transition";
 
+
+
 export default async function AttendancePage() {
   await verifySession();
 
--- app/(auth)/shops/temp/edit/page.tsx
+++ app/(auth)/shops/temp/edit/page.tsx
@@ -20,8 +20,6 @@ import { useState } from "react";
 import { HiArrowLeft, HiSave } from "react-icons/hi";
 import { Animation } from "@/src/components/templates/Animation";
 
-export const runtime = "edge";
-
 // 仮のデータ取得
 const getMockShopData = (id: string) => {
   const shops = {
