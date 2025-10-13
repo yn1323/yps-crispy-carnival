8c5d57a71039e697c3b0712e34f3b3c627508e0c
y-natani <yn1323@gmail.com>
2025-10-13 23:42:52 +0900

    fix

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -1,6 +1,7 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { AuthGuard } from "@/src/components/features/auths/AuthGuard";
+import { BottomMenu } from "@/src/components/templates/BottomMenu";
 import { SideMenu } from "@/src/components/templates/SideMenu";
 
 export const Route = createFileRoute("/_auth")({
@@ -10,11 +11,23 @@ export const Route = createFileRoute("/_auth")({
 function RouteComponent() {
   return (
     <AuthGuard>
-      <Box display="flex">
-        <SideMenu />
-        <Box ml="250px" mt={4} mr={4} flex={1}>
-          <Outlet />
+      <Box display="flex" w="100vw" overflow="hidden">
+        {/* PC: SideMenu */}
+        <Box hideBelow="lg">
+          <SideMenu />
         </Box>
+
+        {/* Content Area */}
+        <Box ml={{ base: 0, lg: "180px" }} flex={1} display="flex" justifyContent="center">
+          <Container maxW="container.xl" py={{ base: 2, lg: 8 }} px={4} mb={{ base: "80px", lg: 0 }} w="100%">
+            <Outlet />
+          </Container>
+        </Box>
+      </Box>
+
+      {/* SP: BottomMenu - flex containerの外に配置 */}
+      <Box hideFrom="lg">
+        <BottomMenu />
       </Box>
     </AuthGuard>
   );
