ea4e35e49d17ec8dcc7a6d11bc753cd30569c2bc
y-natani <yn1323@gmail.com>
2025-11-23 01:21:23 +0900

    fix

--- src/components/pages/Staffs/DetailPage/index.stories.tsx
+++ src/components/pages/Staffs/DetailPage/index.stories.tsx
@@ -1,14 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { MembersDetailPage } from ".";
+import { StaffDetailPage } from ".";
 
 const meta = {
   title: "pages/Members/DetailPage",
-  component: MembersDetailPage,
+  component: StaffDetailPage,
   args: {
     userId: "jh7bg3b58ebkepen0pada606xx7s9aff",
     shopId: "jd7esxeq46sxn5ya9h7q4naaex7s9bkd",
   },
-} satisfies Meta<typeof MembersDetailPage>;
+} satisfies Meta<typeof StaffDetailPage>;
 export default meta;
 
 export const Basic: StoryObj<typeof meta> = {};
