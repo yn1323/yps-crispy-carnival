a7a8f1b193f1e6b1ca9573b0ab4c52c6236b71ef
y-natani <yn1323@gmail.com>
2026-04-05 13:05:17 +0900

    fix

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,11 +1,8 @@
+const key = "CLERK_JWT_ISSUER_DOMAIN";
 export default {
   providers: [
     {
-      // Replace with your own Clerk Issuer URL from your "convex" JWT template
-      // or with `process.env.CLERK_JWT_ISSUER_DOMAIN`
-      // and configure CLERK_JWT_ISSUER_DOMAIN on the Convex Dashboard
-      // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+      domain: process.env[key] ?? "",
       applicationID: "convex",
     },
   ],
