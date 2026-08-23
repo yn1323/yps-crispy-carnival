824caf28027f8693f7b4d4d1b2c99ae68e061541
y-natani <yn1323@gmail.com>
2026-08-23 23:21:58 +0900

    refactor: ActionInboxの操作生成を共通化

--- src/components/features/ActionInbox/index.tsx
+++ src/components/features/ActionInbox/index.tsx
@@ -10,6 +10,7 @@ export type {
   StaffRegistrationActionInboxData,
 } from "./builders";
 export {
+  buildActionInboxAction,
   buildNotificationFailureActionInboxItem,
   buildStaffRegistrationActionInboxItem,
 } from "./builders";
